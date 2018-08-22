bb2f8ac2d1f8e3c8fa594ecbfd756196a311c847
Danni <danni@DannideMacBook-Pro.local>
2018-08-22 11:20:28 +0800

    add

---
+++ src/main/java/com/danni/string/IndexOfDemo.java
@@ -0,0 +1,12 @@
+package com.danni.string;
+
+public class IndexOfDemo {
+	
+	public static void main(String[] args) throws Exception {
+		String s = "abc${yyyyMMdd}.def";
+		System.out.println(s.indexOf("${"));// 3
+		System.out.println(s.indexOf("}"));// 13
+		System.out.println(s.substring(3, 13));// ${yyyyMMdd
+	}
+	
+}
