3660a2ab4c53df8338edd7d1e82a2ce33afad300
danni <danni@130.51.23.57>
2018-09-11 17:21:13 +0800

    non dir

--- src/main/java/java7/CreateFile.java
+++ src/main/java/java7/CreateFile.java
@@ -10,6 +10,26 @@ import java.util.logging.Logger;
 public class CreateFile {
 
 	public static void main(String[] args) throws IOException {
+		createFile();
+		createNonDirAndFile();
+	}
+
+	/**
+	 * 在不存在的目录下创建文件，会抛出异常：
+	 * Exception in thread "main" java.nio.file.NoSuchFileException: ./myfile/UTWX001.txt
+	 */
+	private static void createNonDirAndFile() throws IOException {
+		Path path = Paths.get("./myfile/", "UTWX001.txt");// 创建没有的myfile目录
+		byte[] bytes = "test".getBytes();
+		Files.write(path, bytes);
+		Logger.getGlobal().info("create dir and write file finished.");
+	}
+
+	/**
+	 * 当前目录生成文件
+	 * @throws IOException
+	 */
+	private static void createFile() throws IOException {
 		Path path = Paths.get("./", "UTWX001.txt");// 在src同级目录生成文件
 		byte[] bytes = "test".getBytes();
 		Files.write(path, bytes);
