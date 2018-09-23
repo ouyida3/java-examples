38688d6273cb090932afd3aab92ae66efccd2da0
danni <252283424@qq.com>
2018-09-23 16:08:01 +0800

    thread

---
+++ src/main/java/thread/SimpleThread.java
@@ -0,0 +1,17 @@
+package thread;
+
+/**
+ * 简单的线程类。
+ * 
+ * 在java5之前，大概是这样子的。
+ * 
+ * @author danni
+ * @since 2018.9.23(Sunday)
+ * @date 2018.9.23 我写下这个，是要说明，这样子写，javadoc根本识别不了。
+ * @Date 2018.9.23 如果这样子写，就更可笑了，会指向java.util.Date类。
+ */
+public class SimpleThread {
+	public static void main(String[] args) {
+		Thread thread = new Thread();
+	}
+}
